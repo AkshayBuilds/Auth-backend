e93796769202227ba356db62e55d8dbf5ca966eb
Akshay ChaudharY <akshaychaudhary@Akshays-MacBook-Air.local>
2026-02-28 17:14:14 +0530

    app.js

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
 require("dotenv").config();
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const cookieParser = require('cookie-parser')
-const connectdb = require("./config/db")
-const PORT = process.env.PORT
-const authroutes = require("./routes/authRoutes")
-const userroutes = require('./routes/userRoutes')
 
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-let isconnected = false
+const app = express();
+
+const connectdb = require("./config/db");
+const authroutes = require("./routes/authRoutes");
+const userroutes = require("./routes/userRoutes");
+
+let isConnected = false;
 
 const startServer = async () => {
     try {
-        await connectdb()
-        isconnected = true
-        console.log("MongoDB Connected")
+        await connectdb();
+        isConnected = true;
+        console.log("MongoDB Connected");
     } catch (error) {
-        console.error("Failed to start server:", error)
+        console.error("Failed to start server:", error);
     }
 }
 
-startServer()
+startServer();
 
+const allowedOrigins = [
+  "http://localhost:5173"
+];
 
-const allowedOrigins = ['http://localhost:5173']
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded())
-app.use(cors({origin:allowedOrigins ,credentials: true}))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
-app.get('/', (req,res) => {
-    res.send("api working")
-})
-app.use('/auth', authroutes)
-app.use('/user', userroutes)
+app.get("/", (req, res) => {
+  res.send("api working");
+});
 
+app.use("/auth", authroutes);
+app.use("/user", userroutes);
 
-module.exports = app
+module.exports = app;
